e1788b1680b515671d77e836a824b8cca240dad0
sai sarath kumar <sarathsai30@gmail.com>
2025-06-28 15:32:58 +0530

    added blog width

--- src/components/BlogSection.jsx
+++ src/components/BlogSection.jsx
@@ -79,13 +79,13 @@ const BlogSection = () => {
         </div>
         <div className="row">
           {blogs.slice(0, 3).map(blog => (
-            <div key={blog.id} className="col-lg-4 col-md-6 mb-4">
+            <div key={blog.id} className="col-12 mb-4">
               <div className="card h-100 shadow-sm">
                 <div className="card-body">
                   <h5 className="card-title fw-bold">{blog.title}</h5>
                   <p className="card-text text-muted">
-                    {blog.content.length > 120 
-                      ? `${blog.content.substring(0, 120)}...` 
+                    {blog.content.length > 1000
+                      ? `${blog.content.substring(0, 1000)}...` 
                       : blog.content
                     }
                   </p>
