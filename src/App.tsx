ccdaa171d693a644b8c5fd9ec697da8a7cfafe0c
sai sarath kumar <sarathsai30@gmail.com>
2025-09-07 09:49:13 +0530

    video changes.

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import CarList from './pages/CarList';
 import CarDetail from './pages/CarDetail';
-import OnRoadPrice from './pages/CarVariantsTable/OnRoadPrice';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Admin from './pages/Admin';
@@ -26,7 +25,6 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/cars" element={<CarList />} />
             <Route path="/cars/:id" element={<CarDetail />} />
-            <Route path="/on-road-price/:statename" element={<OnRoadPrice />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/management-dashboard-93cars" element={<Admin />} />
